e7dde383adfa55ee5ccd1c3f291f141d626a77e3
prakash kumar <prakash.1pk20@gmail.com>
2023-05-14 20:47:04 +0530

    added some print statement

--- src/pack1/HelloWorld.java
+++ src/pack1/HelloWorld.java
@@ -4,6 +4,16 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("hello.............");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+		System.out.println(10+20);
+		
+		int a = 5;
+		int b = 10;
+		
+		System.out.println("total " +(a+b));
+		
 		
 	}
 
